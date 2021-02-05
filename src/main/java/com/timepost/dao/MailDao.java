5bf41822b37992f0b2523dcaed2be28ddf5e350a
766521466@qq.com <766521466@qq.com>
2021-02-06 00:36:46 +0800

    添加mapper及dao文件

--- src/main/java/com/timepost/dao/MailDao.java
+++ src/main/java/com/timepost/dao/MailDao.java
@@ -1,6 +1,10 @@
 package com.timepost.dao;
 
+import com.timepost.entity.MailEntity;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Date;
 
 /**
  * @author :  zhulongkun20@gmail.com
@@ -10,4 +14,22 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface MailDao {
+
+    /**
+     * 新增邮件
+     *
+     * @param mailEntity 邮件实体
+     * @return 受影响行数
+     */
+    int insert(MailEntity mailEntity);
+
+    /**
+     * 根据预定发送时间获取邮件
+     *
+     * @param date 预定发送时间
+     * @return 邮件集合
+     */
+    MailEntity selectBySendDate(@Param("date") Date date);
+
 }
+
