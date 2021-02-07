2ae4181715960d08cb79386851a20dfa2f2b8f36
766521466@qq.com <766521466@qq.com>
2021-02-07 23:54:28 +0800

    add db

---
+++ db/dml/v1.0.0/time_post_v1.0.0_insert_t_user_01.sql
@@ -0,0 +1,5 @@
+insert into t_user(id, user_id, role, username, password, mail_address, signature,
+                   last_login, create_by, update_by)
+values ('1170cf1b3f264b2ba6f9f7b05514ee80', 'oumae-kumiko',
+        'ROLE_ADMIN', '黄前久美子', 'kitauji-suisougakubu', '766521466@qq.com',
+        '我会守住北宇治的一切！', '2021-01-01', 'oumae-kumiko', 'oumae-kumiko');
